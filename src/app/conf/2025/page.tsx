f35aadb7a635b25ef6fd38f5b46923aaeecb29af
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-04-24 12:24:01 +0200

    Remove the old Intro section

--- src/app/conf/2025/page.tsx
+++ src/app/conf/2025/page.tsx
@@ -15,22 +15,6 @@ export const metadata: Metadata = {
   title: "GraphQLConf 2025 — Sept 08-10",
 }
 
-function Intro() {
-  return (
-    <section className="flex flex-col gap-20 md:gap-32">
-      <h2 className="text-3xl font-normal lg:text-5xl">
-        Celebrating 10 Years of GraphQL. Three transformative days of expert
-        insights and innovation to shape the next decade of APIs together!
-      </h2>
-
-      <GridButton
-        title="Get Tickets"
-        href="https://cvent.me/PBNYEe?utm_source=graphql_conf_2025&utm_medium=website&utm_campaign=cta"
-      />
-    </section>
-  )
-}
-
 export default function Page() {
   return (
     <main>
@@ -40,7 +24,6 @@ export default function Page() {
         <WhatToExpectSection className="md:mb-8 md:mt-24" />
       </div>
       <div className="container my-20 flex flex-col gap-20 md:my-32 md:gap-32">
-        <Intro />
         <Sponsors />
         <Sponsor />
         <Speakers />
