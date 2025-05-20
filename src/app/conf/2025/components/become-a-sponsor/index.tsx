ccd66a8eb883808b1402cb496bba81f18ed204fb
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-05-20 20:08:45 +0200

    Unify widths per @saihaj's suggestion
    Co-authored-by: Saihajpreet Singh <saihajpreet.singh@gmail.com>

--- src/app/conf/2025/components/become-a-sponsor/index.tsx
+++ src/app/conf/2025/components/become-a-sponsor/index.tsx
@@ -9,7 +9,7 @@ export function BecomeASponsor() {
   return (
     <section id="sponsors" className="relative overflow-hidden">
       <Stripes />
-      <div className="gql-conf-container gql-conf-section xl:py-16 2xl:px-64">
+      <div className="gql-conf-container gql-conf-section xl:py-16">
         <header className="flex flex-col gap-x-48 gap-y-4 md:flex-row md:items-end md:justify-between">
           <div>
             <h2 className="typography-h2">Become a Sponsor</h2>
