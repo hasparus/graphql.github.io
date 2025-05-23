e4dcf59f8c637c26cec8812c38c67bdba94a05a8
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-05-23 18:28:50 +0200

    Improve new location section

--- src/app/conf/_design-system/accordion.tsx
+++ src/app/conf/_design-system/accordion.tsx
@@ -1,10 +1,16 @@
 "use client"
 
+import clsx from "clsx"
+
 import ArrowDown from "@/app/conf/_design-system/pixelarticons/arrow-down.svg?svgr"
+import ExternalLink from "@/app/conf/_design-system/pixelarticons/external-link.svg?svgr"
+
+import { Anchor } from "./anchor"
 
 export interface AccordionItem {
   title: string
   description: React.ReactNode
+  link?: string
 }
 
 export interface AccordionProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -15,35 +21,51 @@ export interface AccordionProps extends React.HTMLAttributes<HTMLDivElement> {
 export function Accordion({
   items,
   multiple = false,
+  className,
   ...rest
 }: AccordionProps) {
   return (
-    <div
-      className="grow space-y-4"
-      onToggle={event => {
-        if (!multiple) {
-          const allDetails = event.currentTarget.querySelectorAll("details")
-          allDetails.forEach(details => {
-            if (details !== event.target) {
-              details.open = false
-            }
-          })
-        }
-      }}
-      {...rest}
-    >
+    <div className={clsx("grow space-y-4", className)} {...rest}>
       {items.map((item, index) => (
         <details
           open={index === 0}
           key={index}
           className="group/q w-full border border-sec-darker @container"
+          onClick={event => {
+            if (!multiple) {
+              const allDetails =
+                event.currentTarget.parentElement?.querySelectorAll("details")
+              if (allDetails) {
+                allDetails.forEach(details => {
+                  console.log(
+                    details,
+                    event.currentTarget,
+                    event.currentTarget === details,
+                  )
+                  if (details !== event.currentTarget) {
+                    details.open = false
+                  }
+                })
+              }
+            }
+          }}
         >
           <summary className="flex cursor-pointer list-none items-center justify-between gap-2 border-sec-darker p-2 px-3 focus:outline-none group-open/q:border-b [&::-webkit-details-marker]:hidden">
             <span className="select-none typography-body-lg">{item.title}</span>
             <ArrowDown className="size-10 shrink-0 text-sec-darker group-open/q:rotate-180" />
           </summary>
-          <div className="text-pretty p-3 typography-body-md">
-            {item.description}
+          <div className="flex items-start p-3">
+            <div className="text-pretty typography-body-md">
+              {item.description}
+            </div>
+            {item.link && (
+              <Anchor
+                href={item.link}
+                className="ml-auto p-2 hover:bg-neu-800/10"
+              >
+                <ExternalLink className="size-6" />
+              </Anchor>
+            )}
           </div>
         </details>
       ))}
