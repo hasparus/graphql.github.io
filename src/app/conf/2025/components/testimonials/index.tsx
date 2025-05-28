02f888cc84a61fbe15a9ab17d6a0570f11a864e7
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-05-28 18:55:55 +0200

    Switch testimonials

--- src/app/conf/2025/components/testimonials/index.tsx
+++ src/app/conf/2025/components/testimonials/index.tsx
@@ -18,29 +18,32 @@ interface Testimonial {
 const testimonials: Testimonial[] = [
   {
     quote:
-      "GraphQL has transformed how we build and scale our APIs at Netflix. By allowing our teams to fetch exactly the data they need, we've improved performance, reduced API complexity, and accelerated development cycles.",
+      "GraphQL is evolving to new use cases every day and it's really a competitive advantage to experience them first hand with everyone that matters. I look forward the next edition!",
     author: {
-      name: "Saihajpreet Singh",
-      role: "The Guild",
-      avatar: "https://avatars.githubusercontent.com/u/44710980?v=4",
+      name: "Vincent Desmares",
+      role: "Teamstarter, CTO",
+      avatar:
+        "https://avatars.sched.co/d/cc/21066875/avatar.jpg.320x320px.jpg?f80",
     },
   },
   {
     quote:
-      "GraphQL has transformed how we build and scale our APIs at Netflix. By allowing our teams to fetch exactly the data they need, we've improved performance, reduced API complexity, and accelerated development cycles.",
+      "As a beginner in GraphQL, it was very helpful to see real use cases and honest accounts of the challenges along the way. I learned a lot about performance and security and had a great opportunity to network with other participants and potential vendors.",
     author: {
-      name: "Saihajpreet Singh",
-      role: "The Guild",
-      avatar: "https://avatars.githubusercontent.com/u/44710980?v=4",
+      name: "Nicolai Draslov",
+      role: "Danish Agency for Digital Government",
+      avatar:
+        "https://media.licdn.com/dms/image/v2/C4E03AQGlrdt3GpJI9w/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1528203207471?e=1753920000&v=beta&t=H6CMhDZFoXJxGUu4XYwC_rEX9Jjwh7OdPIDm8JaeXAU",
     },
   },
   {
     quote:
-      "GraphQL has transformed how we build and scale our APIs at Netflix. By allowing our teams to fetch exactly the data they need, we've improved performance, reduced API complexity, and accelerated development cycles.",
+      "GraphQLConf 24 was well organized event which empowers new and existing organizations to adopt GraphQL and help navigate how to rollout within their organizations by building understanding of ecosystem.",
     author: {
-      name: "Saihajpreet Singh",
-      role: "The Guild",
-      avatar: "https://avatars.githubusercontent.com/u/44710980?v=4",
+      name: "Satish Chitnis",
+      role: "Paramount, Principal Architect",
+      avatar:
+        "https://avatars.sched.co/1/c3/21496512/avatar.jpg.320x320px.jpg?0c2",
     },
   },
 ]
@@ -54,7 +57,7 @@ export function Testimonials({ className, ...rest }: TestimonialsProps) {
       )}
       {...rest}
     >
-      <h2 className="text-center text-neu-800 typography-h2">
+      <h2 className="typography-h2 text-center text-neu-800">
         How was the previous edition?
       </h2>
       <div className="flex w-full snap-x snap-mandatory flex-row gap-10 overflow-x-auto px-4 py-6 lg:mt-16 lg:py-16">
@@ -64,7 +67,7 @@ export function Testimonials({ className, ...rest }: TestimonialsProps) {
             className="flex shrink-0 snap-start flex-row-reverse items-center gap-6 max-md:flex-col md:px-10"
           >
             <div>
-              <p className="max-w-[calc(100vw-32px)] typography-body-lg max-md:text-center md:max-w-[544px] xl:text-2xl">
+              <p className="typography-body-lg max-w-[calc(100vw-32px)] !leading-[1.1] max-md:text-center md:max-w-[544px]">
                 {testimonial.quote}
               </p>
               <AuthorNameAndRole
@@ -83,15 +86,15 @@ export function Testimonials({ className, ...rest }: TestimonialsProps) {
 function TestimonialAuthor({ author }: { author: Testimonial["author"] }) {
   return (
     <div className="relative flex shrink-0 flex-col items-center justify-center whitespace-pre md:px-6 lg:h-full lg:px-8">
-      <div className="relative bg-neu-500 dark:bg-neu-200">
+      <div className="relative bg-neu-500 dark:bg-neu-200 dark:opacity-90">
         <Image
           src={author.avatar}
           alt={author.name}
           width={128}
           height={128}
-          className="size-16 xl:size-32"
+          className="size-16 saturate-0 xl:size-32"
         />
-        <div className="absolute inset-0 z-[1] bg-pri-base opacity-95 mix-blend-plus-lighter" />
+        <div className="absolute inset-0 z-[1] bg-pri-darker mix-blend-plus-lighter" />
         <Stripes />
       </div>
       <AuthorNameAndRole author={author} className="contents md:hidden" />
@@ -112,8 +115,8 @@ function AuthorNameAndRole({
 }) {
   return (
     <div className={className}>
-      <div className="mt-3 typography-body-sm">{author.name}</div>
-      <div className="text-neu-700 typography-body-xs">{author.role}</div>
+      <div className="typography-body-sm mt-3">{author.name}</div>
+      <div className="typography-body-xs text-neu-700">{author.role}</div>
     </div>
   )
 }
