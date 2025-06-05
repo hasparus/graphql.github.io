6036527c4f265cebbd5bd0f15708c2243c26c182
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-06-05 12:58:41 +0200

    Fix speaker card on mobiles

--- src/app/conf/2025/components/speaker-card.tsx
+++ src/app/conf/2025/components/speaker-card.tsx
@@ -45,7 +45,7 @@ export function SpeakerCard({
           />
         )}
 
-        <div className="relative h-full shrink-0 overflow-hidden @[420px]:w-[176px]">
+        <div className="relative h-full overflow-hidden @[420px]:w-[176px] @[420px]:shrink-0">
           <div className="absolute inset-0 z-[1] bg-sec-light mix-blend-multiply" />
           {speaker.avatar ? (
             <Image
