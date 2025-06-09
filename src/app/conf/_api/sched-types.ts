7ae48deb29539ed81a20ce4df5584069baef8110
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-06-09 22:40:55 +0200

    Commit Sched data to JSON files

---
+++ src/app/conf/_api/sched-types.ts
@@ -0,0 +1,31 @@
+export type ScheduleSession = {
+  id: string
+  audience: string
+  description: string
+  event_end: string
+  event_start: string
+  event_subtype: string
+  event_type: string
+  name: string
+  venue: string
+  speakers?: SchedSpeaker[]
+  files?: { name: string; path: string }[]
+}
+
+export type SchedSpeaker = {
+  username: string
+  name: string
+  about: string
+  company?: string
+  position?: string
+  avatar?: string
+  url?: string
+  role?: string
+  location?: string
+  socialurls: { service: string; url: string }[]
+  /* unix timestamp */
+  ["~syncedDetailsAt"]?: number
+  ["~years"]: ConferenceYear[]
+}
+
+export type ConferenceYear = 2025 | 2024 | 2023
