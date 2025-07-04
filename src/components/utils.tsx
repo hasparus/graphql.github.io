f4f22611def23724304c8f70037c74540fe406e5
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-07-04 20:33:06 +0200

    Get rid of ts-expect-error

--- src/components/utils.tsx
+++ src/components/utils.tsx
@@ -4,6 +4,5 @@ export function renderComponent<T>(
 ) {
   if (!ComponentOrNode) return null
   if (typeof ComponentOrNode !== "function") return ComponentOrNode
-  // @ts-expect-error TS2322: Type '{}' is not assignable to type 'T'
-  return <ComponentOrNode {...props} />
+  return <ComponentOrNode {...props!} />
 }
