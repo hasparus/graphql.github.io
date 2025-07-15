e24421d82c4cde4fc0f93a77cba810fbe35aa688
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-07-15 15:26:59 +0200

    Fix Proven Solution mobile styles

--- src/components/index-page/proven-solution/index.tsx
+++ src/components/index-page/proven-solution/index.tsx
@@ -4,58 +4,68 @@ import { SectionLabel } from "@/app/conf/_design-system/section-label"
 import CheckIcon from "@/app/conf/_design-system/pixelarticons/check.svg?svgr"
 
 import blurBean from "./blur-bean.webp"
+import { StripesDecoration } from "@/app/conf/_design-system/stripes-decoration"
 
 export function ProvenSolution() {
   return (
-    <section className="gql-container relative overflow-hidden bg-pri-dark text-white dark:bg-pri-darker xl:py-20">
+    <section className="dark relative overflow-hidden bg-pri-dark text-white dark:bg-pri-darker xl:py-20">
       <Stripes />
-      <div className="gql-section relative">
-        <SectionLabel className="mb-6 !text-sec-light">
-          Business perspective
-        </SectionLabel>
-        <h2 className="typography-h2 mb-6 lg:mb-16">
-          A proven solution for startups and enterprises
-        </h2>
-        <div className="mb-12 grid gap-y-6 lg:grid-cols-3 lg:[&>*:not(:first-child)]:border-l-0">
-          <ProvenSolutionCard
-            title={
-              <>
-                The best user
-                <br className="max-lg:hidden" />
-                experience
-              </>
-            }
-            description="Deliver high-performing user experiences at scale. The world’s leading apps use GraphQL to create faster, more responsive digital experiences."
-            bullets={[
-              "Faster data retrieval and load times",
-              "Improved bandwith efficiency",
-            ]}
-          />
-          <ProvenSolutionCard
-            title={
-              <>
-                Stability &<br className="max-lg:hidden" />
-                Security
-              </>
-            }
-            description="Protect your APIs while maintaining full visibility into data consumption. GraphQL allows you to monitor, secure, and optimize API usage while ensuring compliance."
-            bullets={[
-              "Stronger access control",
-              "Improved business intelligence & cost analysis",
-            ]}
-          />
-          <ProvenSolutionCard
-            title="Efficient distributed development"
-            description="Let your teams ship faster with GraphQL’s flexible, decoupled architecture. GraphQL allows frontend and backend teams to work independently and efficiently."
-            bullets={[
-              "More rapid iterations",
-              "Improved cross-team collaboration",
-            ]}
-          />
+      <div className="gql-container py-8">
+        <div className="gql-section relative">
+          <SectionLabel className="mb-6 !text-sec-light">
+            Business perspective
+          </SectionLabel>
+          <h2 className="typography-h2 mb-6 lg:mb-16">
+            A proven solution for startups and enterprises
+          </h2>
+          <div className="mb-12 grid gap-y-6 lg:grid-cols-3 lg:backdrop-blur-[6.4px] lg:[&>*:not(:first-child)]:border-l-0">
+            <ProvenSolutionCard
+              title={
+                <>
+                  The best user{" "}
+                  <br className="@[530px]/card:hidden max-lg:hidden" />
+                  experience
+                </>
+              }
+              description="Deliver high-performing user experiences at scale. The world’s leading apps use GraphQL to create faster, more responsive digital experiences."
+              bullets={[
+                "Faster data retrieval and load times",
+                "Improved bandwith efficiency",
+              ]}
+            />
+            <ProvenSolutionCard
+              title={
+                <>
+                  Stability &{" "}
+                  <br className="@[530px]/card:hidden max-lg:hidden" />
+                  Security
+                </>
+              }
+              description="Protect your APIs while maintaining full visibility into data consumption. GraphQL allows you to monitor, secure, and optimize API usage while ensuring compliance."
+              bullets={[
+                "Stronger access control",
+                "Improved business intelligence & cost analysis",
+              ]}
+            />
+            <ProvenSolutionCard
+              title={
+                <>
+                  Efficient distributed{" "}
+                  <br className="@[530px]/card:hidden max-lg:hidden" />
+                  development
+                </>
+              }
+              description="Let your teams ship faster with GraphQL’s flexible, decoupled architecture. GraphQL allows frontend and backend teams to work independently and efficiently."
+              bullets={[
+                "More rapid iterations",
+                "Improved cross-team collaboration",
+              ]}
+            />
+          </div>
+          <Button className="mx-auto mt-8 w-fit lg:mt-16" href="/learn">
+            Learn more
+          </Button>
         </div>
-        <Button className="mx-auto mt-8 w-fit lg:mt-16" href="/learn">
-          Learn more
-        </Button>
       </div>
     </section>
   )
@@ -71,8 +81,10 @@ function ProvenSolutionCard({
   bullets: string[]
 }) {
   return (
-    <div className="flex flex-col border border-pri-light bg-pri-lighter/20 backdrop-blur-md dark:bg-pri-base/20">
-      <h3 className="typography-h3 border-b border-pri-light p-6">{title}</h3>
+    <div className="flex flex-col border border-pri-light bg-pri-lighter/20 @container/card dark:bg-pri-base/20 max-lg:backdrop-blur-[6.4px]">
+      <h3 className="border-b border-pri-light p-6 text-2xl @[331px]/card:typography-h3">
+        {title}
+      </h3>
       <p className="typography-body-lg p-6">{description}</p>
       <ul className="typography-body-md mt-auto flex flex-col gap-2 p-6 pt-0">
         {bullets.map(bullet => (
@@ -86,17 +98,14 @@ function ProvenSolutionCard({
   )
 }
 
-const maskEven =
-  "repeating-linear-gradient(to right, transparent, transparent 12px, black 12px, black 24px)"
-const maskOdd =
-  "repeating-linear-gradient(to right, black, black 12px, transparent 12px, transparent 24px)"
-
 function Stripes() {
+  // todo: rotate to top right corner on mobile
   return (
     <ImageLoaded
       role="presentation"
       image={blurBean}
-      className="pointer-events-none absolute inset-x-0 bottom-[-385px] top-[-203px] translate-y-12 opacity-0 transition duration-[400ms] ease-linear [mask-position:70%_60%] [mask-size:cover] data-[loaded=true]:translate-y-0 data-[loaded=true]:opacity-100 max-3xl:[mask-size:220%] max-md:[mask-size:800%]"
+      // eslint-disable-next-line tailwindcss/no-unnecessary-arbitrary-value
+      className="pointer-events-none absolute inset-0 opacity-0 transition-[translate] duration-[400ms] ease-linear [mask-position:center_calc(100%+65vw)] [mask-size:200%] data-[loaded=true]:opacity-100 max-lg:rotate-[180deg] max-lg:scale-x-[-1] lg:[mask-position:center_500px] lg:[mask-size:150%] xl:translate-y-1/2 xl:[mask-position:center_top] xl:max-3xl:translate-y-[50%]"
       style={{
         maskImage: `url(${blurBean.src})`,
         WebkitMaskImage: `url(${blurBean.src})`,
@@ -104,30 +113,10 @@ function Stripes() {
         WebkitMaskRepeat: "no-repeat",
       }}
     >
-      {/* TODO: StripesDecoration with proper colors for dark mode. */}
-      {/* light-1: background: linear-gradient(180deg, rgba(255, 204, 239, 0.20) 0%, var(--Primary-Base, #E10098) 100%);
-       */}
-      {/* light-2: background: linear-gradient(180deg, var(--Primary-Light, #FF99DF) 0%, var(--Primary-Lighter, #FFCCEF) 100%); */}
-      {/* dark-1: background: linear-gradient(180deg, var(--Primary-Base, #E10098) 0%, var(--Primary-Darker, #660046) 100%);
-// dark 2: background: linear-gradient(180deg, var(--Primary-Darker, #660046) 0%, var(--Primary-Base, #E10098) 100%);
- */}
-      <div
-        className="absolute inset-0"
-        style={{
-          backgroundImage:
-            "linear-gradient(180deg, hsl(var(--color-pri-light)) 0%, hsl(var(--color-pri-lighter)) 100%)",
-          maskImage: maskEven,
-          WebkitMaskImage: maskEven,
-        }}
-      />
-      <div
-        className="absolute inset-0"
-        style={{
-          backgroundImage:
-            "linear-gradient(180deg,hsl(319deg 100% 90% / 0.2) 0%, hsl(var(--color-pri-base)) 100%)",
-          maskImage: maskOdd,
-          WebkitMaskImage: maskOdd,
-        }}
+      {/* todo: ensure colors in dark mode are correct */}
+      <StripesDecoration
+        evenClassName="bg-[linear-gradient(180deg,hsl(var(--color-pri-light)/0.2)_20%,hsl(var(--color-pri-base))_150%)] dark:bg-[linear-gradient(180deg,hsl(var(--color-pri-base))_20%,hsl(var(--color-pri-darker))_150%)]"
+        oddClassName="bg-[linear-gradient(180deg,hsl(var(--color-pri-light))_20%,hsl(var(--color-pri-lighter)/.9)_150%)] dark:bg-[linear-gradient(180deg,hsl(var(--color-pri-darker)/.9)_20%,hsl(var(--color-pri-base)/.8)_150%)]"
       />
     </ImageLoaded>
   )
