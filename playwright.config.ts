de8dec5e4435fa9c3ac7bc7ea896b3069cda133e
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-08-06 23:51:25 +0200

    Add tests for MiniGraphiQL

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,7 @@ import { defineConfig, devices } from "@playwright/test"
  */
 export default defineConfig({
   testDir: "./test/e2e",
+  outputDir: "./test/out",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
