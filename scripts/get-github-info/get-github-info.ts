0455e0ef42947905bc213524b9f54a650c480857
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-09-10 17:37:14 +0200

    improve the GitHub star sync script (#2134)
    * Move to scripts directory
    
    * Don't repeat GitHub star syncs more often than every two hours

--- scripts/get-github-info/get-github-info.ts
+++ scripts/get-github-info/get-github-info.ts
@@ -4,12 +4,30 @@ import grayMatter from "gray-matter"
 import {
   getGitHubStats,
   type GitHubInfo,
-} from "../scripts/sort-libraries/get-github-stats"
+} from "../sort-libraries/get-github-stats"
+
+const DATA_PATH = new URL("./github-stats.json", import.meta.url).pathname
+const LAST_RUN_PATH = new URL("./last-success.isodate", import.meta.url)
+  .pathname
+const CODE_DIR = new URL("../../src/code", import.meta.url).pathname
 
 async function main() {
-  const filePaths = await fg("./src/code/**/*.md")
+  const filePaths = await fg("./**/*.md", { cwd: CODE_DIR, absolute: true })
 
   const errors: Error[] = []
+
+  {
+    // we only sync once every two hours
+    const TWO_HOURS = 2 * 60 * 60 * 1000
+    const lastRun = await fs.readFile(LAST_RUN_PATH, "utf8").catch(() => "")
+    const twoHoursAgo = new Date(Date.now() - TWO_HOURS)
+    if (lastRun && new Date(lastRun).getTime() > twoHoursAgo.getTime()) {
+      console.info(
+        "Skipping sync of GitHub stars, last run was within two hours.",
+      )
+      return
+    }
+  }
 
   const newState = new Map<string /* repo name */, GitHubInfo>()
   const filePathToRepoName = new Map<
@@ -57,8 +75,7 @@ async function main() {
   // If it errored for some reason, we don't do anything.
   // If we got it, we overwrite.
   {
-    const dataPath = "./src/github-stats.json"
-    const data = await fs.readFile(dataPath, "utf8")
+    const data = await fs.readFile(DATA_PATH, "utf8")
     const existingStats = JSON.parse(data) as Record<string, object>
 
     const result: Record<string, object> = {}
@@ -79,7 +96,8 @@ async function main() {
       result[repoName] = newState.get(repoName)!
     }
 
-    await fs.writeFile(dataPath, JSON.stringify(result, null, 2))
+    await fs.writeFile(DATA_PATH, JSON.stringify(result, null, 2))
+    await fs.writeFile(LAST_RUN_PATH, new Date().toISOString())
   }
 }
 
