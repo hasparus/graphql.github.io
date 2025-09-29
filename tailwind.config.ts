964477fb5a219367d80b20d4b25c41ebfedc623e
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-09-29 23:06:33 +0200

    redesigned blog (#2162)
    * wip
    
    * add blog header
    
    * Make DataColocation narrower on very large screens
    
    * get styles closer to designs
    
    * Improve focus management
    
    * Handle longer bylines
    
    * Add hover styles
    
    * wip
    
    * Implement BlogCardPicture
    
    * Use oklch
    
    * Make it pop
    
    * Reduce jitter
    
    * Optimize and tweak styles
    
    * Fade in
    
    * Actually fade in
    
    * Format
    
    * temporary: add leva to let the designers pick aspectRatio
    
    * Point Prettier to our Tailwind config
    
    * Improve mobile styles
    
    * Update blog layout just a bit
    
    * Remove temporary code, the designers picked aspectRatio=4.75
    
    * Set aspect-ratio to 4.75 and refactor out a weird selector
    
    * Make tags on /blog links, hide featured posts on /tags/* pages
    
    * Add a bit more space
    
    * Update lockfile

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -80,6 +80,7 @@ const config: Config = {
           "arrow-left var(--animation-duration, .75s) var(--animation-direction, forwards) ease infinite",
         "show-overflow":
           "show-overflow var(--animation-duration, 12s) var(--animation-delay, 1s) var(--animation-direction, forwards) ease infinite",
+        "fade-in": "fade-in var(--animation-duration, 200ms) ease-out forwards",
       },
       keyframes: {
         scroll: {
@@ -102,6 +103,10 @@ const config: Config = {
           "25%, 75%": {
             transform: "translateX(var(--delta-x))",
           },
+        },
+        "fade-in": {
+          from: { opacity: "0" },
+          to: { opacity: "1" },
         },
       },
     },
