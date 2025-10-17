53cfc96a4a67bc98ce7df887b15faf17f53436a3
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-10-17 20:27:13 +0200

    Improve hover style again (aniimation was too much)

--- src/app/conf/_design-system/pixelarticons/radio-icon.tsx
+++ src/app/conf/_design-system/pixelarticons/radio-icon.tsx
@@ -13,7 +13,7 @@ export function RadioIcon({ checked, ...rest }: RadioIconProps) {
       <path d="M17 3H7v2H5v2H3v10h2v2h2v2h10v-2h2v-2h2V7h-2V5h-2V3zm0 2v2h2v10h-2v2H7v-2H5V7h2V5h10z" />
       <path
         d="M8 11h2v2h2v2h-2v-2H8v-2zm8-2h-2v2h-2v2h2v-2h2V9z"
-        className="fill-transparent [[data-checked]_&]:fill-current [[data-unchecked]:hover_&]:animate-pulse [label:hover_&]:fill-current"
+        className="fill-transparent [[data-checked]_&]:fill-current [[data-unchecked]:hover_&]:opacity-70 [label:hover_&]:fill-current"
       />
     </svg>
   )
