2625c187f6ce143f5f72de51a433ed5f6d5d9e31
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-10-20 15:31:28 +0200

    Perfect styles

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -71,12 +71,15 @@ export function ThemeSwitch({ lite, className }: ThemeSwitchProps) {
                   <Select.Item
                     key={option}
                     value={option}
-                    className="mt-px flex cursor-pointer items-center gap-2 px-3 py-1.5 text-neu-800 first:mt-0 focus-visible:outline-none focus-visible:ring-0 data-[highlighted]:bg-neu-100 data-[highlighted]:text-neu-900 data-[selected]:text-neu-900 dark:text-neu-700 dark:data-[highlighted]:bg-neu-50"
+                    className="mt-px flex cursor-pointer items-center gap-2 px-3 py-1.5 text-neu-800 first:mt-0 focus-visible:outline-none focus-visible:ring-0 data-[highlighted]:bg-neu-100 data-[highlighted]:text-neu-900 data-[selected]:text-neu-600 dark:text-neu-700 dark:data-[highlighted]:bg-neu-50"
                   >
                     <Icon
                       height={
                         // the icons are not fully proportional
                         Icon === SystemIcon ? "18" : "16"
+                      }
+                      className={
+                        Icon === SystemIcon ? "-mr-0.5 -translate-x-px" : ""
                       }
                     />
                     <Select.ItemText className="capitalize leading-none">
