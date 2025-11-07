f2b01a1e569f2e90a5a73a6172931100632d8601
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-11-07 15:21:16 +0100

    Initialize Leaflet once to avoid noisy errors

--- src/app/(main)/community/events/meetups.tsx
+++ src/app/(main)/community/events/meetups.tsx
@@ -1,36 +1,59 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 
 import "leaflet/dist/leaflet.css"
 import { EventsScrollview } from "./events-scrollview"
 import { meetups } from "../../../../components/meetups"
 import { EventCard } from "./event-card"
 
-import pinkCircle from "./pink-cirle.svg"
+import pinkCircle from "./pink-circle.svg"
 
 export function Meetups() {
+  const mapContainer = useRef<HTMLDivElement>(null)
+  const mapRef = useRef<import("leaflet").Map>()
+  const loadingTokenRef = useRef<symbol>()
+
   useEffect(() => {
-    // Load only on client
-    import("leaflet").then(L => {
+    if (mapRef.current) return
+    const loadingToken = (loadingTokenRef.current = Symbol())
+    ;(async function loadMap() {
+      if (!mapContainer.current) return
+      const Leaflet = await import("leaflet")
+
+      if (loadingToken !== loadingTokenRef.current) {
+        return
+      }
+
       // Fixes GET http://localhost:3000/community/upcoming-events/marker-icon-2x.png 404 (Not Found)
       // and replace default marker image
-      L.Icon.Default.mergeOptions({
+      Leaflet.Icon.Default.mergeOptions({
         iconRetinaUrl: pinkCircle.src,
         shadowUrl: "",
       })
-      const map = L.map("map").setView([45, -15], 2)
-      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map)
+
+      const map = Leaflet.map(mapContainer.current).setView([45, -15], 2)
+      mapRef.current = map
+
+      Leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
+        map,
+      )
+
       for (const { node } of meetups) {
-        L.marker([node.latitude, node.longitude])
+        Leaflet.marker([node.latitude, node.longitude])
           .addTo(map)
           .bindPopup(
             `<a href="${node.link}" target="_blank" rel="noreferrer" class="!text-primary">${node.name}</a>`,
           )
       }
-    })
+    })()
+
+    return () => {
+      if (mapRef.current) mapRef.current.remove()
+      mapRef.current = undefined
+    }
   }, [])
   return (
     <>
-      <div id="map" className="z-0 my-6 h-96" />
+      <div ref={mapContainer} className="z-0 my-6 h-96" />
       <EventsScrollview>
         {meetups.map(({ node }) => (
           <EventCard
