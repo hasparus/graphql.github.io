18650f2947014a8de30548db65014b615c37fba2
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-11-26 22:05:50 +0100

    wip

--- src/components/learn-aggregator/items.tsx
+++ src/components/learn-aggregator/items.tsx
@@ -2,150 +2,116 @@ import meta from "../../pages/learn/_meta"
 
 type LearnPagePath = Exclude<keyof typeof meta, `-- ${string}` | "index">
 
-function getTitle(path: LearnPagePath): string {
-  const entry = meta[path as keyof typeof meta]
-  if (typeof entry === "string") {
-    return (
-      entry || path.replace(/-/g, " ").replace(/\b\w/g, c => c.toUpperCase())
-    )
-  } else if (typeof entry === "object" && "title" in entry) {
-    return entry.title
-  }
-  return path.replace(/-/g, " ").replace(/\b\w/g, c => c.toUpperCase())
+interface LearnPageItem {
+  title: string
+  description: string
+  icon: string
+  section: "getting-started" | "best-practices"
 }
 
-export const learnPages: Record<
-  LearnPagePath,
-  {
-    title: string
-    description: string
-    icon: string
-    section: "getting-started" | "best-practices"
-  } | null
-> = {
+const _items: Record<LearnPagePath, Omit<LearnPageItem, "title"> | null> = {
   introduction: {
-    title: getTitle("introduction"),
     description: "",
     icon: "",
     section: "getting-started",
   },
   schema: {
-    title: getTitle("schema"),
     description: "",
     icon: "",
     section: "getting-started",
   },
   queries: {
-    title: getTitle("queries"),
     description: "",
     icon: "",
     section: "getting-started",
   },
   mutations: {
-    title: getTitle("mutations"),
     description: "",
     icon: "",
     section: "getting-started",
   },
   subscriptions: {
-    title: getTitle("subscriptions"),
     description: "",
     icon: "",
     section: "getting-started",
   },
   validation: {
-    title: getTitle("validation"),
     description: "",
     icon: "",
     section: "getting-started",
   },
   execution: {
-    title: getTitle("execution"),
     description: "",
     icon: "",
     section: "getting-started",
   },
   response: {
-    title: getTitle("response"),
     description: "",
     icon: "",
     section: "getting-started",
   },
   introspection: {
-    title: getTitle("introspection"),
     description: "",
     icon: "",
     section: "getting-started",
   },
   // ---
   "best-practices": {
-    title: getTitle("best-practices"),
     description: "",
     icon: "",
     section: "best-practices",
   },
   "thinking-in-graphs": {
-    title: getTitle("thinking-in-graphs"),
     description: "",
     icon: "",
     section: "best-practices",
   },
   "serving-over-http": {
-    title: getTitle("serving-over-http"),
     description: "",
     icon: "",
     section: "best-practices",
   },
   "file-uploads": {
-    title: getTitle("file-uploads"),
     description: "",
     icon: "",
     section: "best-practices",
   },
   authorization: {
-    title: getTitle("authorization"),
     description: "",
     icon: "",
     section: "best-practices",
   },
   pagination: {
-    title: getTitle("pagination"),
     description: "",
     icon: "",
     section: "best-practices",
   },
   "schema-design": {
-    title: getTitle("schema-design"),
     description: "",
     icon: "",
     section: "best-practices",
   },
   "global-object-identification": {
-    title: getTitle("global-object-identification"),
     description: "",
     icon: "",
     section: "best-practices",
   },
   caching: {
-    title: getTitle("caching"),
     description: "",
     icon: "",
     section: "best-practices",
   },
   performance: {
-    title: getTitle("performance"),
     description: "",
     icon: "",
     section: "best-practices",
   },
   security: {
-    title: getTitle("security"),
     description: "",
     icon: "",
     section: "best-practices",
   },
   federation: {
-    title: getTitle("federation"),
     description: "",
     icon: "",
     section: "best-practices",
@@ -153,4 +119,22 @@ export const learnPages: Record<
   // ---
   // omitted on Learn index page
   "debug-errors": null,
+}
+
+export const learnPages = _items as Record<LearnPagePath, LearnPageItem | null>
+
+for (const path in learnPages) {
+  const page = learnPages[path as LearnPagePath]
+  if (page === null) continue
+  const metaEntry = meta[path as keyof typeof meta]
+
+  if (typeof metaEntry === "string") {
+    page.title =
+      metaEntry ||
+      path.replace(/-/g, " ").replace(/\b\w/g, c => c.toUpperCase())
+  } else if (typeof metaEntry === "object" && "title" in metaEntry) {
+    page.title = metaEntry.title
+  } else {
+    page.title = path.replace(/-/g, " ").replace(/\b\w/g, c => c.toUpperCase())
+  }
 }
