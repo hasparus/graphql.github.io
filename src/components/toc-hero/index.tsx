24d4f66a9a919aa6a96229002246ff0173fb2adc
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-11-26 19:59:22 +0100

    Finalize TocHero styles

--- src/components/toc-hero/index.tsx
+++ src/components/toc-hero/index.tsx
@@ -1,35 +1,20 @@
-import CaretDown from "@/app/conf/_design-system/pixelarticons/caret-down.svg?svgr"
+export { TocHeroContents } from "./toc-hero-contents"
 
 export interface TocHeroProps {
   heading: string
+  text: React.ReactNode
+  children: React.ReactNode
+  decoration: React.ReactNode
 }
-export function TocHero({ heading }: TocHeroProps) {
+export function TocHero({ heading, text, children, decoration }: TocHeroProps) {
   return (
-    <section className="flex flex-col gap-6 lg:gap-8">
-      <h1 className="typography-h1">{heading}</h1>
+    <section className="relative bg-neu-0 pt-[calc(var(--nextra-navbar-height)+18px)]">
+      {decoration}
+      <div className="gql-section gql-container relative flex !max-w-screen-lg flex-col items-center gap-6 text-center lg:gap-8">
+        <h1 className="typography-h1">{heading}</h1>
+        <p className="typography-body-sm max-w-[80vw] text-pretty">{text}</p>
+        {children}
+      </div>
     </section>
-  )
-}
-
-export function TocHeroContents({ ids }: { ids: string[] }) {
-  return (
-    <div className="">
-      <h2 className="typography-body-lg px-4 py-2.5 text-center md:py-4">
-        What will you find here?
-      </h2>
-      <ul className="grid grid-flow-row-dense grid-rows-2 gap-px bg-neu-300">
-        {ids.map((id, i) => (
-          <li className="" key={i}>
-            <a
-              className="flex gap-3 bg-neu-0 has-[a:hover]:bg-neu-50"
-              href={`#${id}`}
-            >
-              <CaretDown className="size-4 translate-x-[0.5px] -rotate-90" />
-              {id}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
   )
 }
